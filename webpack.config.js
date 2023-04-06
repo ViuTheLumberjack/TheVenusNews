4cae068eb43b2b125e2891f1ed20437d2d176c90
silviu48 <silviu48vatamanelu@gmail.com>
2023-04-06 14:09:06 +0200

    Added copy for public assets

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -33,6 +34,11 @@ const config = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "static", to: "public" },
+      ],
     }),
 
     // Add your plugins here
