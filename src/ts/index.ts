c872e5c2b8344ba118be46ca5135bf744036795c
silviu48 <silviu48vatamanelu@gmail.com>
2023-04-17 21:04:01 +0200

    Header Kinda Desktop Ready

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,3 +3,17 @@ import '../styles/main.scss';
 console.log("Hello World!");
 
 console.log("Chi me lo ha fatto fare?!?");
+
+document.getElementById("site_header").addEventListener("scroll", adjustHeader);
+
+function adjustHeader(){
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("site_header").classList.remove("large");
+        document.getElementById("site_header").classList.add("not_top");
+      } else {
+        document.getElementById("site_header").classList.remove("not_top");
+        document.getElementById("site_header").classList.add("large");
+      }
+      
+      console.log("ok");
+}
