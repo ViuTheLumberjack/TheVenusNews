abec4dde772617ae8c80b0fdd203e877185b5bc9
silviu48 <silviu48vatamanelu@gmail.com>
2023-04-18 21:35:04 +0200

    A little javascript needed

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,17 +1,37 @@
 import '../styles/main.scss';
 
-console.log("Hello World!");
-
-console.log("Chi me lo ha fatto fare?!?");
-
-document.getElementById("site_header").addEventListener("scroll", adjustHeader);
+document.addEventListener("DOMContentLoaded", function(event) {
+	window.onscroll =  adjustHeader;
+	document.getElementById("close_breaking").addEventListener("click", close);
+	document.getElementById("cookies").addEventListener("click", openCookiePolicy);
+});
 
 function adjustHeader(){
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("site_header").classList.remove("large");
-        document.getElementById("site_header").classList.add("not_top");
-      } else {
-        document.getElementById("site_header").classList.remove("not_top");
-        document.getElementById("site_header").classList.add("large");
-      }
+	var header = document.getElementById("site_header");
+	var navbar = document.getElementById("navbar");
+	var trending = document.getElementById("trending");
+	var shrinkHeader = 60;
+	var scroll = getCurrentScroll();
+
+	if ( scroll >= shrinkHeader ) {
+		header.classList.add('small');
+		navbar.classList.add('hidden');
+		trending.classList.add('hidden');
+	} else {
+		header.classList.remove('small');
+		navbar.classList.remove('hidden');
+		trending.classList.remove('hidden');
+	}
+}
+
+function getCurrentScroll() {
+	return window.scrollY || document.documentElement.scrollTop;
+}
+
+function close(){
+	document.getElementById("breaking_news").classList.add('hidden');
+}
+
+function openCookiePolicy(){
+	// document.getElementById("cookie_policy").classList.remove('hidden');
 }
