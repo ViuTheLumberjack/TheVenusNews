dc241a711d52a872366a87aa133b0d243ae10680
silviu48 <silviu48vatamanelu@gmail.com>
2023-04-19 11:33:02 +0200

    Not very polished but works

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,13 +1,47 @@
 import '../styles/main.scss';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-	window.onscroll =  adjustHeader;
+	window.onscroll = adjustHeader;
 	document.getElementById("close_breaking").addEventListener("click", close);
 	document.getElementById("cookies").addEventListener("click", openCookiePolicy);
+	document.getElementById("menu").addEventListener("click", openMenu);
+	document.getElementById("search").addEventListener("click", openSearchBar);
+
+	document.getElementById("accordion_like").childNodes.forEach(function(element){
+		// check if element has an i tag
+		element.childNodes.forEach(function(child){
+			if(child.nodeName == "I"){
+				element.addEventListener("click", showAccordion);
+			}
+		});
+
+		
+		
+	});
 });
 
+function showAccordion(){
+	var itag = this as HTMLElement;
+	var ul = itag.childNodes[5] as HTMLElement;
+
+	ul.style.display = "block";
+
+	itag.removeEventListener("click", showAccordion);
+	itag.addEventListener("click", closeAccordion);
+}
+
+function closeAccordion(){
+	var itag = this as HTMLElement;
+	var ul = itag.childNodes[5] as HTMLElement;
+
+	ul.style.display = "none";
+
+	itag.addEventListener("click", showAccordion);
+	itag.removeEventListener("click", closeAccordion);
+}
+
 function adjustHeader(){
-	var header = document.getElementById("site_header");
+	var header = document.getElementById("header_wrapper");
 	var navbar = document.getElementById("navbar");
 	var trending = document.getElementById("trending");
 	var shrinkHeader = 60;
@@ -33,5 +67,80 @@ function close(){
 }
 
 function openCookiePolicy(){
-	// document.getElementById("cookie_policy").classList.remove('hidden');
+	var page = document.getElementById("page");
+	document.body.classList.add('shadow');
+	document.body.classList.add('noscroll');
+	document.getElementById("cookie_policy").classList.remove('hidden');
+	page.addEventListener("click", closeCookiePolicy);
+	document.getElementById("cookie_button").addEventListener("click", closeCookiePolicy);
+}
+
+function closeCookiePolicy(){
+	var page = document.getElementById("page");
+	document.body.classList.remove('shadow');
+	document.getElementById("site_header").classList.remove('shadow');
+	document.body.classList.remove('noscroll');
+	page.removeEventListener("click", closeCookiePolicy);
+	document.getElementById("cookie_policy").classList.add('hidden');
+}
+
+function openMenu(){
+	var navbar = document.getElementById("navbar");
+	var page = document.getElementById("page");
+	document.getElementById("header_wrapper").classList.add("small");
+	navbar.classList.add('vertical');
+	navbar.classList.remove('hidden');
+	document.body.classList.add('shadow');
+	document.body.classList.add('noscroll');
+	page.addEventListener("click", closeMenu);
+	document.getElementById("menu").addEventListener("click", closeMenu);
+	document.getElementById("menu").removeEventListener("click", openMenu);
+	consumeEvent(window.event);
+}
+
+function closeMenu(){
+	var navbar = document.getElementById("navbar");
+	var page = document.getElementById("page");
+	document.getElementById("header_wrapper").classList.remove("small");
+	navbar.classList.remove('vertical');
+	document.body.classList.remove('shadow');
+	document.body.classList.remove('noscroll');
+	page.removeEventListener("click", closeMenu);
+	document.getElementById("menu").removeEventListener("click", closeMenu);
+	document.getElementById("menu").addEventListener("click", openMenu);
+}
+
+function openSearchBar(){
+	var navbar = document.getElementById("navbar");
+	var searchbar = document.getElementById("searchbar");
+	var page = document.getElementById("page");
+	document.getElementById("header_wrapper").classList.add("small");
+	navbar.classList.add('hidden');
+	searchbar.classList.remove('hidden');
+	document.body.classList.add('shadow');
+	document.body.classList.add('noscroll');
+	page.addEventListener("click", closeSearchBar);
+	document.getElementById("search").addEventListener("click", closeSearchBar);
+	document.getElementById("search").removeEventListener("click", openSearchBar);
+	consumeEvent(window.event);
+}
+
+function closeSearchBar(){
+	var navbar = document.getElementById("navbar");
+	var searchbar = document.getElementById("searchbar");
+	var page = document.getElementById("page");
+	document.getElementById("header_wrapper").classList.remove("small");
+	navbar.classList.remove('hidden');
+	searchbar.classList.add('hidden');
+	document.body.classList.remove('shadow');
+	document.body.classList.remove('noscroll');
+	page.removeEventListener("click", closeSearchBar);
+	document.getElementById("search").removeEventListener("click", closeSearchBar);
+	document.getElementById("search").addEventListener("click", openSearchBar);
+}
+
+function consumeEvent(event: Event) {
+	event.stopImmediatePropagation();
+	event.stopPropagation();
+	event.preventDefault();
 }
